5ab57f0a1e10dad9876b2ed000d80d449f15da94
claytonketner <claytonketner@me.com>
2015-02-07 22:42:53 -0800

    Added index

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -5,8 +5,9 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'portfolio.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+
+    url(r'^$', 'portfolio.views.index', name='index'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
