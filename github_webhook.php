3e2fe3b8141292888b9752ac7b87183471cae98e
claytonketner <claytonketner@me.com>
2015-02-07 22:57:55 -0800

    Cleanup

--- github_webhook.php
+++ github_webhook.php
@@ -1,14 +1,20 @@
 <?php
 
+// Pull and touch
 shell_exec("cd ~/website");
 $output = shell_exec("git pull");
 echo $output;
 shell_exec("touch dispatch.fcgi");
 shell_exec("touch tmp/restart.txt");
 
+// Rebuild static
+shell_exec("rm -r portfolio/static");
+shell_exec("source .virtualenv/bin/activate");
+shell_exec("./manage.py collectstatic --noinput");
+
 // Set permissions
 $perms = array(
-    "700 github_webhook.php",
+    "700 cleanup.php",
     "700 dispatch.fcgi",
     "600 portfolio/settings/key.py",
     "600 portfolio/settings/secret.py",
@@ -17,7 +23,3 @@ foreach ($perms as $perm)
 {
     echo shell_exec("chmod " . $perm);
 }
-
-shell_exec("rm -r portfolio/static");
-shell_exec("source .virtualenv/bin/activate");
-shell_exec("./manage.py collectstatic --noinput");
