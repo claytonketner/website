5c17d1638dbcb6b5d447942907b356e2b454cb77
claytonketner <claytonketner@me.com>
2015-02-07 18:32:05 -0800

    Set permissions too

--- github_webhook.php
+++ github_webhook.php
@@ -2,6 +2,18 @@
 
 shell_exec("cd ~/website");
 $output = shell_exec("git pull");
+echo $output;
 shell_exec("touch dispatch.fcgi");
 shell_exec("touch tmp/restart.txt");
-echo $output;
+
+// Set permissions
+perms = array(
+    "700 github_webhook.php",
+    "700 dispatch.fcgi",
+    "600 portfolio/settings/key.py",
+    "600 portfolio/settings/secret.py",
+);
+foreach ($perms as $perm)
+{
+    echo shell_exec("chmod " . $perm);
+}
