03188c6d5306345e9008a6668bf74dc3e21b8c24
claytonketner <claytonketner@me.com>
2015-02-28 23:29:59 -0800

    Added blog view

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,9 +1,25 @@
+from django.db.models import F
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from blog.models import Blog
+
 
 def index(request):
-    context = {}
-    context['message'] = "I'm switching this site to Django in " \
-                         "my free time. It might take a while!"
-    return render(request, 'portfolio/index.html', context)
+    return blog(request)
+
+def blog(request, target_blog_slug=None):
+    target_blog = Blog.objects.get(is_index=True)
+    if target_blog_slug:
+        target_blog = Blog.objects.get(slug=target_blog_slug)
+
+    target_blog.views = F('views') + 1
+    target_blog.save()
+    blog_entries = target_blog.blogentry_set.all()
+    blogs = Blog.objects.all()
+    context = {
+        'current_blog': target_blog,
+        'blog_entries': blog_entries,
+        'blogs': blogs,
+    }
+    return render(request, 'portfolio/blog.html', context)
