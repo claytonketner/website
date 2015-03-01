e3166563b47fd4a8373153131eeac73e11ba727f
claytonketner <claytonketner@me.com>
2015-02-28 23:30:40 -0800

    Added blog urls

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,13 +1,17 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
+
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^$', 'portfolio.views.index', name='index'),
+urlpatterns = patterns(
+    '',
+    url(r'^$',
+        'portfolio.views.index',
+        name='index'),
+    url(r'^blog/(?P<target_blog_slug>[\w\-]+)/$',
+        'portfolio.views.blog',
+        name='blog'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
